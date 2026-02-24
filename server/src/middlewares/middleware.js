e11543275780ac0ef5026ad6a2facc4aa2a3b9b5
Niraj Shah <nirajshah950@gmail.com>
2026-02-24 23:05:45 +0530

    create the middleware function to call the middlewares.

---
+++ server/src/middlewares/middleware.js
@@ -0,0 +1,11 @@
+import cors from "cors";
+import { json } from "express";
+import appRouter from "../route/app.route.js";
+
+const middlwares = (app) => {
+  app.use(json());
+  app.use(cors());
+  app.use(appRouter);
+};
+
+export default middlwares;
