729cdf0dd538f54d143c72f976a64bae8bab9a4a
Niraj Shah <nirajshah950@gmail.com>
2026-02-24 23:05:54 +0530

    Define the custom Error class.

---
+++ server/src/utility/CustomError.js
@@ -0,0 +1,13 @@
+class CustomError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = false;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default CustomError;
